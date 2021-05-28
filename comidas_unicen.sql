e5f56e650962b2703d2b451165c1736fc666483f
Reynoso, María Del Mar <reynosomariadelmar@gmail.com>
2021-05-27 22:06:44 -0300

    Modificación del Home

---
+++ comidas_unicen.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-05-2021 a las 10:08:49
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `comidas_unicen`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorías`
+--
+
+CREATE TABLE `categorías` (
+  `id_categoria` int(11) NOT NULL,
+  `nombres` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `categorías`
+--
+
+INSERT INTO `categorías` (`id_categoria`, `nombres`) VALUES
+(1, 'ENTRADAS/MINUTAS'),
+(2, 'PLATO PRINCIPAL'),
+(3, 'POSTRES');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `platos`
+--
+
+CREATE TABLE `platos` (
+  `id_plato` int(11) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `descripción` text NOT NULL,
+  `id_nacionalidad` varchar(200) NOT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `platos`
+--
+
+INSERT INTO `platos` (`id_plato`, `nombre`, `descripción`, `id_nacionalidad`, `id_categoria`) VALUES
+(1, 'Bruschetta', 'Consiste en rebanadas de pan tostado, rebozadas con algún ajo y puestas a la parrilla para que se doren', 'italia', 1),
+(2, 'Focaccia de cebolla', 'La focaccia es una especie de pan plano cubierto con hierbas y otros productos alimenticios.​​Se trata de un plato tradicional de la cocina italiana muy relacionado con la popular pizza', 'Italia', 1),
+(3, 'Lasaña rellena tradicional', 'La lasaña es un tipo de pasta. Se suele servir en láminas superpuestas intercaladas con capas de ingredientes al gusto, más frecuentemente carne en salsa boloñesa', 'Italia', 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorías`
+--
+ALTER TABLE `categorías`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `platos`
+--
+ALTER TABLE `platos`
+  ADD PRIMARY KEY (`id_plato`),
+  ADD KEY `fk_categoria_plato` (`id_categoria`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorías`
+--
+ALTER TABLE `categorías`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `platos`
+--
+ALTER TABLE `platos`
+  MODIFY `id_plato` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `platos`
+--
+ALTER TABLE `platos`
+  ADD CONSTRAINT `fk_categoria_plato` FOREIGN KEY (`id_categoria`) REFERENCES `categorías` (`id_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- templates/home.tpl
+++ templates/home.tpl
@@ -4,3 +4,13 @@
     
         {include file="nav.tpl"}
     </div>
+<article class="Bienvenida">
+<h1 class="titulo_bienvenida"> ¡Bienvenidos al Restourante UNICEN!</h1>
+<p class="text"> ¿Querés experimentar nuevos sabores? En nuestro restourante cuenta con una amplia variedades de platos
+¡Ven a probarlos!</p>
+<p class="text"> Te ofrecemos un menú con platos típicos de distintas nacionalidades </p>
+<p class="text"> Puedes ingresar para conocer nuestra carta de <a href="carta"> nuestra carta </a> y elegir el menú
+más delicio </p>
+</article>
+
+{include file="footer.tpl"}
